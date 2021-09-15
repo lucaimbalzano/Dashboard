a759d8baa07f7df4994fb5bd73974bc7ecdc0f94
luca <lucaimbalzano@gmail.com>
2021-09-15 11:11:32 +0200

    ## Making Structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DefaultComponent } from './layout/default/default.component';
+import { DashboardComponent } from './module/dashboard/dashboard.component';
+import { DefaultModule } from './layout/default/default.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    DefaultComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    DefaultModule
   ],
   providers: [],
   bootstrap: [AppComponent]
