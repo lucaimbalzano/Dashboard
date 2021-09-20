a33a7d84a80ea14e320346eda3aa06176232e8c2
luca <lucaimbalzano@gmail.com>
2021-09-20 10:26:18 +0200

    ## Data Highchart Example inserted

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
@@ -11,7 +8,14 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatListModule } from '@angular/material/list'
 import { RouterModule } from '@angular/router';
+import { HighchartsChartModule } from 'highcharts-angular';
+
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { AreaComponent } from './widgets/area/area.component';
+
+
 
 @NgModule({
   declarations: [
@@ -29,7 +33,8 @@ import { AreaComponent } from './widgets/area/area.component';
     MatButtonModule,
     MatMenuModule,
     FlexLayoutModule,
-    MatListModule
+    MatListModule,
+    HighchartsChartModule
   ],
   exports:[
     HeaderComponent,
